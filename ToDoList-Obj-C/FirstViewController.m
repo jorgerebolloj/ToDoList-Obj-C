717a733e123d550ff198e17389291a7181fcf24e
Jorge Rebollo <jorge.rebollo@pademobile.com>
2016-02-04 10:00:11 -0600

    implemented delete TODO

--- ToDoList-Obj-C/FirstViewController.m
+++ ToDoList-Obj-C/FirstViewController.m
@@ -82,26 +82,76 @@
     return cellView;
 }
 
-- (NSArray *)rightButtons
-{
+- (NSArray *)rightButtons {
     NSMutableArray *rightUtilityButtons = [NSMutableArray new];
     [rightUtilityButtons sw_addUtilityButtonWithColor:[UIColor clearColor] icon:[UIImage imageNamed:@"edit64x64.png"]];
     [rightUtilityButtons sw_addUtilityButtonWithColor:[UIColor clearColor] icon:[UIImage imageNamed:@"delete64x64.png"]];
-    
     return rightUtilityButtons;
 }
 
-- (NSArray *)leftButtons
-{
+- (NSArray *)leftButtons {
     NSMutableArray *leftUtilityButtons = [NSMutableArray new];
-    
     [leftUtilityButtons sw_addUtilityButtonWithColor:[UIColor clearColor] icon:[UIImage imageNamed:@"email64x64.png"]];
     [leftUtilityButtons sw_addUtilityButtonWithColor:[UIColor clearColor] icon:[UIImage imageNamed:@"sms64x64.png"]];
     [leftUtilityButtons sw_addUtilityButtonWithColor:[UIColor clearColor] icon:[UIImage imageNamed:@"facebook64x64.png"]];
     [leftUtilityButtons sw_addUtilityButtonWithColor:[UIColor clearColor] icon:[UIImage imageNamed:@"twitter64x64.png"]];
-    
     return leftUtilityButtons;
 }
+
+// click event on left utility button
+- (void)swipeableTableViewCell:(SWTableViewCell *)cell didTriggerLeftUtilityButtonWithIndex:(NSInteger)index {
+    switch (index) {
+        case 0:
+            NSLog(@"email button was pressed");
+            break;
+        case 1:
+            NSLog(@"sms button was pressed");
+            break;
+        case 2:
+            NSLog(@"facebook button was pressed");
+            break;
+        case 3:
+            NSLog(@"twitter button was pressed");
+        default:
+            break;
+    }
+}
+
+// click event on right utility button
+- (void)swipeableTableViewCell:(SWTableViewCell *)cell didTriggerRightUtilityButtonWithIndex:(NSInteger)index {
+    switch (index) {
+        case 0:
+            NSLog(@"Edit button was pressed");
+            break;
+        case 1:
+        {
+            NSLog(@"Delete button was pressed");
+            NSIndexPath *cellIndexPath = [self.toDoPendingListTable indexPathForCell:cell];
+            [self.toDoPendingListViewModel removeObjectAtIndex:cellIndexPath.row];
+            [self.toDoPendingListTable deleteRowsAtIndexPaths:@[cellIndexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+            [[NSUserDefaults standardUserDefaults] setObject:self.toDoPendingListViewModel forKey:@"toDoPendingList"];
+            [[NSUserDefaults standardUserDefaults] synchronize];
+            self.toDoPendingListViewModel = [[[NSUserDefaults standardUserDefaults] arrayForKey:@"toDoPendingList"] mutableCopy];
+            [self.toDoPendingListTable reloadData];
+            break;
+        }
+        default:
+            break;
+    }
+}
+
+// utility button open/close event
+- (void)swipeableTableViewCell:(SWTableViewCell *)cell scrollingToState:(SWCellState)state {
+    
+}
+
+// prevent multiple cells from showing utilty buttons simultaneously
+/*- (BOOL)swipeableTableViewCellShouldHideUtilityButtonsOnSwipe:(SWTableViewCell *)cell {
+}
+
+// prevent cell(s) from displaying left/right utility buttons
+- (BOOL)swipeableTableViewCell:(SWTableViewCell *)cell canSwipeToState:(SWCellState)state {
+}*/
 
 #pragma mark UITable Delegate
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
