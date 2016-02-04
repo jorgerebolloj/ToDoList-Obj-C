1129b8cc251f3e2052006cc61fdc406b79865822
Jorge Rebollo <jorge.rebollo@pademobile.com>
2016-02-04 00:17:06 -0600

    Feature number 1 DONE

--- ToDoList-Obj-C/FirstViewController.m
+++ ToDoList-Obj-C/FirstViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "FirstViewController.h"
+#import "ToDoPendingListTableViewCell.h"
 
 @interface FirstViewController ()
 
@@ -16,12 +17,53 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self.toDoPendingListTable setDelegate:self];
+    [self.toDoPendingListTable setDataSource:self];
+    self.toDoPendingListViewModel = [NSMutableArray arrayWithArray:@[
+                                                              @{
+                                                                  @"title"      : @"Buy milk",
+                                                                  @"modifiedDate" : @"12/02/2016",
+                                                                  @"status"    : @0,
+                                                                  @"image"    : @"image512x512.png",
+                                                                  },
+                                                              @{
+                                                                  @"title"      : @"Learn Swift",
+                                                                  @"modifiedDate" : @"12/02/2016",
+                                                                  @"status"    : @0,
+                                                                  @"image"    : @"image512x512.png",
+                                                                  },
+                                                              @{
+                                                                  @"title"      : @"Play violin",
+                                                                  @"modifiedDate" : @"12/02/2016",
+                                                                  @"status"    : @0,
+                                                                  @"image"    : @"image512x512.png",
+                                                                  },
+                                                              ]];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark UITableViewDataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.toDoPendingListViewModel count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cellView;
+    ToDoPendingListTableViewCell *toDoPendingTableViewCell = [tableView dequeueReusableCellWithIdentifier:@"ToDoPendingListViewCell" forIndexPath:indexPath];
+    SEL selector = @selector(setToDoPendingListModel:);
+    if([toDoPendingTableViewCell respondsToSelector:selector]) {
+        NSMutableDictionary *toDoPendingCellViewModel = [self.toDoPendingListViewModel objectAtIndex:indexPath.row];
+        [toDoPendingTableViewCell setToDoPendingListModel:toDoPendingCellViewModel];
+    }
+    cellView = toDoPendingTableViewCell;
+    cellView.backgroundColor = [UIColor clearColor];
+    cellView.backgroundView = [[UIImageView alloc] init];
+    cellView.selectedBackgroundView = [[UIImageView alloc] init];
+    return cellView;
 }
 
 @end
